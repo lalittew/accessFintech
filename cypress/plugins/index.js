bb3316fcfbeac01ed9c07b971660daf66a62d307
Lalit Tewari <lalittew@gmail.com>
2023-03-31 12:21:40 +0100

    Added HTML reporting plugins for generating HTML report , added video capability and retry capability

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,16 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
-		module.exports = (on, config) => {
-		on("file:preprocessor", cucumber());
-		};
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
+module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
+  on("file:preprocessor", cucumber());
+  on('before:run', async(details) => {
+      console.log('override before:run');
+      await beforeRunHook(details);
+  });
+
+  on('after:run', async() => {
+      console.log('override after:run');
+      await afterRunHook();
+  });
+}
